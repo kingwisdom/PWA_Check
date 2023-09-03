e2fa8127f8c21f7f2ff95c2be8abb0a46c190570
Tayo <temitayo.adeoye@innotelligent.com>
2023-09-03 10:19:16 +0100

    try deploy

--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -1,7 +1,61 @@
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <>
+            <div id="carouselExample" className="carousel slide">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src="https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?cs=srgb&dl=pexels-miguel-%C3%A1-padri%C3%B1%C3%A1n-255379.jpg&fm=jpg" className="d-block w-100" alt="https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?cs=srgb" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_640.jpg" className="d-block w-100" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://img.freepik.com/premium-photo/searchlight-neon-brick-wall-with-smoke-neon-reflections-wet-asphalt-empty-scene-with-copy-space_117255-1836.jpg" className="d-block w-100" alt="..." />
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true" />
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true" />
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4 col-sm-12 mb-3 mb-sm-0">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Special title treatment</h5>
+                                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 col-sm-12">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Special title treatment</h5>
+                                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 col-sm-12">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Special title treatment</h5>
+                                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
--- src/swDev.js
+++ src/swDev.js
@@ -1,6 +1,31 @@
 export default function swDev() {
+    function determineAppServerKey() {
+        var vapidPublicKey = 'BE163iUYgUivxIK69yViEuiBIa-oQ u-kkmHU89$tffB'
+        return urlBase64ToUint8Array(vapidPublicKey);
+    }
+    function urlBase64ToUint8Array(base64String) {
+        const padding = '='.repeat((4 - base64String.length % 4) % 4);
+        const base64 = (base64String + padding)
+            .replace(/-/g, '+')
+            .replace(/_/g, '/');
+
+        const rawData = window.atob(base64);
+        const outputArray = new Uint8Array(rawData.length);
+
+        for (let i = 0; i < rawData.length; ++i) {
+            outputArray[i] = rawData.charCodeAt(i);
+        }
+        return outputArray
+    }
     let swUrl = `${process.env.PUBLIC_URL}/sw.js`
     navigator.serviceWorker.register(swUrl).then((res) => {
         console.warn("response", res)
+        return res.pushManager.getSubscription()
+            .then(function (suscription) {
+                return res.pushManager.subscribe({
+                    userVisibleOnly: true,
+                    applicationServerKey: determineAppServerKey
+                })
+            })
     })
 }
