7a651afeddfd8b07db7b452b57452877baf229f8
Tayo <temitayo.adeoye@innotelligent.com>
2023-09-03 12:46:55 +0100

    deploy to host

--- src/screen/Header.jsx
+++ src/screen/Header.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
-
 const Header = () => {
     return (
         <Navbar bg="primary" data-bs-theme="dark">
             <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Navbar.Brand href="/">Navbar</Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link href="/">Home</Nav.Link>
                     <Nav.Link href="/about">About</Nav.Link>
